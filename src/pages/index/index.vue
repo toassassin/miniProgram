3ad20d442ec0c1d6dcfe6abd5e53e8004643336c
dengxiaowei <2499553036@qq.com>
2018-08-27 18:46:49 +0800

    commit code

---
+++ src/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+
+    components: {},
+    onLoad() {
+        this.getData();
+    },
+    methods: {
+        async getData() {
+            var data = await this.$http.get(
+                "http://47.106.115.52:19091/index/getHeader?prefix=cd"
+            );
+            console.log(data);
+        },
+        bindViewTap() {
+            const url = "../logs/main";
+            wx.navigateTo({ url });
+        },
+        getUserInfo() {
+            // 调用登录接口
+            wx.login({
+                success: () => {
+                    wx.getUserInfo({
+                        success: res => {
+                            this.userInfo = res.userInfo;
+                        }
+                    });
+                }
+            });
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
