76caeb20713a48ae307f6b98bd3820660cdfa8f1
dengxiaowei <2499553036@qq.com>
2018-08-28 01:42:03 +0800

    commit code v2

---
+++ src/common/iconItem.vue
@@ -0,0 +1,23 @@
+<template>
+	<navigator url="">
+        <view class='weui-grid__bg'>
+            <view class="weui-grid__icon">
+                <image :src="url" mode="scaleToFill" />
+            </view>
+            <text class="weui-grid__label">{{text}}</text>
+        </view>
+    </navigator>
+</template>
+<script>
+	export default {
+		props:["url","text"],
+		data(){
+			return {
+
+			}
+		}
+	};
+</script>
+<style scoped>
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,63 @@
 <template>
   <div>
-
+    <section>
+        <div class="navbar-top">
+            <div>
+                <navigator url="../logs/main">请老师</navigator>
+            </div>
+            <div>
+                <navigator url="../counter/main">当老师</navigator>
+            </div>
+        </div>
+    </section>
+    <section>
+        <swiper class="swiper" indicator-dots="true" autoplay="true" circular="true" interval="3000" duration="500" @change="change($event)">
+            <block v-for="(item,index) in imgs" :index="index" :key="key">
+                <swiper-item>
+                    <image :src="item" class="slide-image" mode="scaleToFill" width="100%" height="100%" />
+                </swiper-item>
+            </block>
+        </swiper>
+    </section>
+    <section>
+        <view class="nav-box">
+            <navitem :url="url_1" :text="11"></navitem>
+            <navitem :url="url_2" :text="22"></navitem>
+            <navitem :url="url_2" :text="22"></navitem>
+            <navitem :url="url_2" :text="22"></navitem>
+        </view>
+        <view class="nav-box">
+            <navitem :url="url_1" :text="11"></navitem>
+            <navitem :url="url_2" :text="22"></navitem>
+            <navitem :url="url_2" :text="22"></navitem>
+            <navitem :url="url_2" :text="22"></navitem>
+        </view>
+    </section>
   </div>
 </template>
 
 <script>
+import navitem from "../../common/iconitem.vue";
 export default {
-    data() {
-        return {};
+    components:{
+        navitem
     },
-
-    components: {},
+    data() {
+        return {
+            url_1:"/static/img/p1.png",
+            url_2:"/static/img/p2.png",
+            // imgs:[2,4,5,6,6]
+            imgs:["https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1535385248&di=7716fc5c62cbe4cb35c49961fac20e79&src=http://pic9.photophoto.cn/20081128/0033033999061521_b.jpg","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535395199954&di=91e1a397937692f7ebaa2719d2858c12&imgtype=0&src=http%3A%2F%2Fpic23.photophoto.cn%2F20120503%2F0034034456597026_b.jpg","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535395199954&di=579371f867c3eba33130d24d8be5bff2&imgtype=0&src=http%3A%2F%2Fpic22.photophoto.cn%2F20120225%2F0034034432152602_b.jpg"
+            ]
+        };
+    },
     onLoad() {
         this.getData();
     },
     methods: {
+        change(event){
+            console.log(event.mp.detail.current)
+        },
         async getData() {
             var data = await this.$http.get(
                 "http://47.106.115.52:19091/index/getHeader?prefix=cd"
@@ -42,4 +85,26 @@ export default {
 </script>
 
 <style scoped>
+.navbar-top{
+    display: flex;
+    justify-content: space-between;
+    /*box-shadow: 0 5px 10px rgba(0,0,0,.3);*/
+}
+.navbar-top>div{
+    flex: 1;
+    text-align: center;
+}
+.navbar-top>div navigator{
+    height: 80rpx;
+    line-height: 80rpx;
+}
+.swiper .slide-image{
+    width: 100%;
+    height: 100%;
+}
+.nav-box{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
 </style>
